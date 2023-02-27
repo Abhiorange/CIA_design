cc1358ffd67fbea1861e153b841d01f00b321398
Abhiorange <soniabhi0510@gmail.com>
2023-02-27 18:25:42 +0530

    volunteer and story

--- volunteer.js
+++ volunteer.js
@@ -72,3 +72,18 @@ function removeElement(value) {
     filtersSection.removeChild(elementToBeRemoved);
 
 }
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
